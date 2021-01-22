faeb3dcd97f4ba5d9a7747c9c9adb010a5d3a56f
”HT234Y” <id09856@gmail.com>
2021-01-22 17:51:37 +0200

    was complited compute line: age

--- person/models/persons.py
+++ person/models/persons.py
@@ -7,8 +7,8 @@ class Persons(models.Model):
     _name = 'person.person'
     _description = 'person.person'
 
-    first_name = fields.Char()
-    last_name = fields.Char()
+    first_name = fields.Char(required=True)
+    last_name = fields.Char(required=True)
     full_name = '{0} {1}'.format(first_name, last_name)
     birthday = fields.Date(string='Dates')
     age = fields.Integer(compute='_compute_age')
@@ -16,4 +16,10 @@ class Persons(models.Model):
         ('male', 'Male'), 
         ('female', 'Female'), 
         ('non-binary', 'Non-binary')])
-    company_id = fields.Many2one('res.company')
+    company_id = fields.Many2one('res.company', required=True)
+
+    def _compute_age(self):
+        if self.birthday:
+            d1 = self.birthday
+            d2 = date.today()
+            self.age = relativedelta(d2, d1).years
