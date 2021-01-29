61b516f5644c79a3cf69ca07d3788f81f185a1ea
”HT234Y” <id09856@gmail.com>
2021-01-29 12:01:40 +0200

    trying to do something

--- person/controllers/controllers.py
+++ person/controllers/controllers.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Person(http.Controller):
-#     @http.route('/person/person/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Person(http.Controller):
+    # @http.route('/person/person/', auth='public')
+    # def index(self, **kw):
+    #     return "Hello, world"
 
-#     @http.route('/person/person/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('person.listing', {
-#             'root': '/person/person',
-#             'objects': http.request.env['person.person'].search([]),
-#         })
+    @http.route('/person/person/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('person.listing', {
+            'root': '/person/person',
+            'objects': http.request.env['person.person'].search([]),
+        })
 
-#     @http.route('/person/person/objects/<model("person.person"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('person.object', {
-#             'object': obj
-#         })
+    @http.route('/person/person/objects/<model("person.person"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('person.object', {
+            'full_name': full_name,
+            'sex': sex,
+            'age': age,
+        })
